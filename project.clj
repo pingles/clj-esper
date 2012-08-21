7dab3b9ae9044b45fb6f1282c0151d5bd95ad8de
Paul Ingles <paul@oobaloo.co.uk>
2012-08-21 08:35:49 +0100

    update project.clj for leiningen 2, releasing 1.0.1

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
-(defproject clj-esper "1.0.0-SNAPSHOT"
-  :description "Simple Esper wrapper for Clojure"
+(defproject clj-esper/clj-esper "1.0.1" 
+  :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
-                 [com.espertech/esper "4.3.0" :exclusions [log4j]]])
+                 [com.espertech/esper "4.3.0" :exclusions [log4j]]]
+  :description "Simple Esper wrapper for Clojure")

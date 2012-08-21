99c932a1df33913c940a7b221e131d1f4e8eddd1
Paul Ingles <paul@oobaloo.co.uk>
2012-08-21 08:38:52 +0100

    using snapshot release in preparation for any further changes

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-esper/clj-esper "1.0.1" 
+(defproject clj-esper/clj-esper "1.0.2-SNAPSHOT" 
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.4.0"]
                  [com.espertech/esper "4.3.0" :exclusions [log4j]]]
